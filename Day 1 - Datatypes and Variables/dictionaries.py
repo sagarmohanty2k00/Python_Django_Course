6c53f80acc8ac91051bed91158a8ae71d98bf432
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2021-12-10 21:33:48 +0530

    List Tuples and Dictionaries done

---
+++ Day 1 - Datatypes and Variables/dictionaries.py
@@ -0,0 +1,6 @@
+d = {}
+
+d["name"] = "sagar"
+d[3] = 3
+
+print(d)
---
+++ Day 1 - Datatypes and Variables/list.py
@@ -0,0 +1,15 @@
+l = []
+
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+l.append('k')
+l.insert(4, 5)
+l.pop()
+l.reverse()
+l.sort()
+l.extend([1, 2])
+l.append([1, 2])
+
+print(l)
